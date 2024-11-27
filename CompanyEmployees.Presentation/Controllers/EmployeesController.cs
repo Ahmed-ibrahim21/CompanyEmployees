9880124da25b07402c02e5c994515d9de7fc2a52
Ahmed-ibrahim21 <ai.ahmed.ibrahim21@gmail.com>
2024-11-27 23:43:23 +0200

    Handled Validation For some POST , PUT & PATCH Requests

--- CompanyEmployees.Presentation/Controllers/EmployeesController.cs
+++ CompanyEmployees.Presentation/Controllers/EmployeesController.cs
@@ -41,6 +41,9 @@ namespace CompanyEmployees.Presentation.Controllers
             if (employee == null)
                 return BadRequest("EmployeeForCreationDto object is null");
 
+            if (!ModelState.IsValid)
+                return UnprocessableEntity(ModelState);
+
             var employeeToReturn = _service.EmployeeService.CreateEmployeeForCompany(companyId, employee,trackChanges: false);
 
             return CreatedAtRoute("GetEmployeeForCompany",new {companyId=companyId,employeeId = employeeToReturn.id},employeeToReturn); 
@@ -59,6 +62,10 @@ namespace CompanyEmployees.Presentation.Controllers
         {
             if (employee is null)
                 return BadRequest("EmployeeForUpdateDto object is null");
+            
+            if (!ModelState.IsValid)
+                return UnprocessableEntity(ModelState);
+
             _service.EmployeeService.UpdateEmployeeForCompany(companyId, id, employee,
             compTrackChanges: false, empTrackChanges: true);
             return NoContent();
@@ -72,7 +79,12 @@ namespace CompanyEmployees.Presentation.Controllers
 
             var result = _service.EmployeeService.GetEmployeeForPatch(companyId, id,compTrackChanges: false,empTrackChanges: true);
 
-            PatchDoc.ApplyTo(result.employeeToPatch);
+            PatchDoc.ApplyTo(result.employeeToPatch,ModelState);
+            
+            TryValidateModel(result.employeeToPatch);
+
+            if (!ModelState.IsValid)
+                return UnprocessableEntity(ModelState);
 
             _service.EmployeeService.SaveChangesForPatch(result.employeeToPatch, result.employeeEntity);
 
