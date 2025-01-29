85aa159994890853210a194d94668fa2864fabc9
Ahmed Ibrahim <ai.ahmed.ibrahim21@gmail.com>
2025-01-30 01:59:53 +0200

    Added Root Document

--- CompanyEmployees/Extensions/ServiceExtensions.cs
+++ CompanyEmployees/Extensions/ServiceExtensions.cs
@@ -51,22 +51,26 @@ public static class ServiceExtensions
         services.Configure<MvcOptions>(config =>
         {
             var systemTextJsonOutputFormatter = config.OutputFormatters
-                    .OfType<SystemTextJsonOutputFormatter>()?.FirstOrDefault();
+            .OfType<SystemTextJsonOutputFormatter>()?.FirstOrDefault();
 
             if (systemTextJsonOutputFormatter != null)
             {
                 systemTextJsonOutputFormatter.SupportedMediaTypes
                 .Add("application/vnd.codemaze.hateoas+json");
+                systemTextJsonOutputFormatter.SupportedMediaTypes
+                .Add("application/vnd.codemaze.apiroot+json");
             }
 
             var xmlOutputFormatter = config.OutputFormatters
-                    .OfType<XmlDataContractSerializerOutputFormatter>()?
-                    .FirstOrDefault();
+              .OfType<XmlDataContractSerializerOutputFormatter>()?
+              .FirstOrDefault();
 
             if (xmlOutputFormatter != null)
             {
                 xmlOutputFormatter.SupportedMediaTypes
                 .Add("application/vnd.codemaze.hateoas+xml");
+                xmlOutputFormatter.SupportedMediaTypes
+                .Add("application/vnd.codemaze.apiroot+xml");
             }
         });
     }
